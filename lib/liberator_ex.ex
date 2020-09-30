33aa88b141f080313ab2f96b2911b5d6d82561f2
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-09-30 10:20:48 -0600

    New mix project

---
+++ lib/liberator_ex.ex
@@ -0,0 +1,18 @@
+defmodule LiberatorEx do
+  @moduledoc """
+  Documentation for LiberatorEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiberatorEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/liberator_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule LiberatorExTest do
+  use ExUnit.Case
+  doctest LiberatorEx
+
+  test "greets the world" do
+    assert LiberatorEx.hello() == :world
+  end
+end
