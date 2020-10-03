1b998b66c5c4258b1a318985a6b13557a0c75495
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-02 20:28:29 -0600

    Extract the decision tree and evaluator function

--- lib/liberator/trace.ex
+++ lib/liberator/trace.ex
@@ -1,10 +1,18 @@
 defmodule Liberator.Trace do
   import Plug.Conn
-  
+
+  @moduledoc """
+  Decision tracing functions.
+  """
+
+  @doc """
+  Get the log of all decisions made on the given conn.
+  """
   def get_trace(conn) do
     Map.get(conn.private, :liberator_trace, [])
   end
 
+  @doc false
   def update_trace(conn, next_step, result) do
     current_trace = get_trace(conn)
     updated_trace = current_trace ++ [{next_step, result}]
