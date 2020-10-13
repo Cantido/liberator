0fa7fcb9a4b1748b57909a1bae8f9e29492bc162
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-10-12 18:56:36 -0600

    Remove mox from dependencies
    We weren't using it!

--- mix.exs
+++ mix.exs
@@ -67,7 +67,6 @@ defmodule Liberator.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:jason, "~> 1.2"},
       {:timex, "~> 3.5"},
-      {:mox, "~> 1.0", only: :test},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
