21809bcecec82e987e5b3de42e5e09770b8723b4
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-05 23:52:34 -0600

    Remove more module defs from tests

--- test/liberator/resource_codec_test.exs
+++ test/liberator/resource_codec_test.exs
@@ -3,25 +3,27 @@ defmodule Liberator.ResourceCodecTest do
   use ExUnit.Case
   use Plug.Test
 
-  test "if a media type codec does not return a binary, throws an exception with a nice message" do
-    defmodule BrokenMediaType do
-      def encode!(_), do: %{a: 1, b: 2}
-    end
+  defmodule BrokenMediaType do
+    def encode!(_), do: %{a: 1, b: 2}
+  end
 
+  defmodule BadMediaTypeCodecResource do
+    use Liberator.Resource
+
+    @impl true
+    def handle_ok(_), do: %{a: 1, b: 2}
+  end
+
+  test "if a media type codec does not return a binary, throws an exception with a nice message" do
     media_types = Application.fetch_env!(:liberator, :media_types)
+
     on_exit(fn ->
       Application.put_env(:liberator, :media_types, media_types)
     end)
+
     Application.put_env(:liberator, :media_types, %{
       "text/plain" => BrokenMediaType
     })
-
-    defmodule BadMediaTypeCodecResource do
-      use Liberator.Resource
-
-      @impl true
-      def handle_ok(_), do: %{a: 1, b: 2}
-    end
 
     expected_message = """
     The media type codec module Liberator.ResourceCodecTest.BrokenMediaType did not return a binary.
@@ -37,25 +39,27 @@ defmodule Liberator.ResourceCodecTest do
     end
   end
 
-  test "if compression codec does not return a binary, throws an exception with a nice message" do
-    defmodule BrokenEncoding do
-      def encode!(_), do: %{a: 1, b: 2}
-    end
+  defmodule BrokenEncoding do
+    def encode!(_), do: %{a: 1, b: 2}
+  end
 
+  defmodule BadCompressionCodecResource do
+    use Liberator.Resource
+
+    @impl true
+    def handle_ok(_), do: %{a: 1, b: 2}
+  end
+
+  test "if compression codec does not return a binary, throws an exception with a nice message" do
     encodings = Application.fetch_env!(:liberator, :encodings)
+
     on_exit(fn ->
       Application.put_env(:liberator, :encodings, encodings)
     end)
+
     Application.put_env(:liberator, :encodings, %{
       "identity" => BrokenEncoding
     })
-
-    defmodule BadCompressionCodecResource do
-      use Liberator.Resource
-
-      @impl true
-      def handle_ok(_), do: %{a: 1, b: 2}
-    end
 
     expected_message = """
     The compression codec module Liberator.ResourceCodecTest.BrokenEncoding did not return a binary.
